d42468e8f4a52565d45645d58a4f76f6494f78a8
mgah77 <mauricio.gah@gmail.com>
2026-01-02 17:10:44 -0300

    fix

--- static/src/js/systray.js
+++ static/src/js/systray.js
@@ -1,19 +1,21 @@
 odoo.define('insumar_sp.SystrayMenu', function (require) {
     "use strict";
 
-    const { Component } = owl;
-    const { useService } = owl.hooks;
-    const SystrayMenu = require('web.SystrayMenu');
-    const { registry } = require('@web/core/registry');
+    const { Component, useState } = owl;
+    const { useService } = require("@web/core/utils/hooks"); // Importación correcta en Odoo 16
+    const { registry } = require("@web/core/registry");
 
     class InsumarSpMenu extends Component {
         setup() {
-            this.rpc = useService('rpc');
-            this.actionService = useService('action');
-            this.state = { counter: 0 };
+            this.rpc = useService("rpc");
+            this.actionService = useService("action");
+            
+            // Usamos useState para que la vista reaccione automáticamente al cambio
+            this.state = useState({ counter: 0 });
+            
             this.updateCounter();
             
-            // Consultar al servidor cada 60 segundos
+            // Polling cada 60 segundos
             setInterval(() => {
                 this.updateCounter();
             }, 60000);
@@ -25,7 +27,7 @@ odoo.define('insumar_sp.SystrayMenu', function (require) {
                 method: 'get_systray_sp_count',
             });
             this.state.counter = count;
-            this.render(); // Re-renderizar para actualizar el número
+            // No necesitamos llamar render() manualmente con useState
         }
 
         _onClickSpRequests() {
@@ -40,12 +42,11 @@ odoo.define('insumar_sp.SystrayMenu', function (require) {
         }
     }
 
-    // Definir el template y los eventos
+    // Asignamos el template XML
     InsumarSpMenu.template = 'insumar_sp.SystrayMenu';
-    InsumarSpMenu.props = {};
-
-    // Agregar al menú del sistema
-    SystrayMenu.Items.push(InsumarSpMenu);
+    
+    // Registramos el widget en el menú del sistema (Systray) de Odoo 16
+    registry.category("systray").add("insumar_sp.SystrayMenu", InsumarSpMenu, { sequence: 1 });
 
     return InsumarSpMenu;
 });
